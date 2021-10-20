4fd1fcec83cfeccf65a86a1e1cf22f7bda62c3dd
Apoorv Vyavahare <82765893+vital987@users.noreply.github.com>
2021-10-20 16:08:16 +0530

    Update ngrok_x64.sh

--- ngrok_x64.sh
+++ ngrok_x64.sh
@@ -1,4 +1,4 @@
-TOKEN="1Y14GB7E4acXxWYnVTiBejgnLuV_853z7mAgaTJxE9KY3HnCW" && \
+TOKEN="1T750atJi3xccndeUqJ4ewiS62o_2s6f8GUccL1qDUXTGSftN" && \
 apt-get update && apt-get install -y wget unzip  && \
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip -P /tmp && \
 unzip /tmp/ngrok-stable-linux-amd64.zip -d /usr/bin && \
